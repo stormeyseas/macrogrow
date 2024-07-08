5cb382540d495910cd11df8116f171aed24f6c4e
Tormey Reimer <tormeyreimer@gmail.com>
2024-07-08 15:34:49 +1000

    Add error messages to I_lim and algae height

--- R/algae_height.R
+++ R/algae_height.R
@@ -8,6 +8,17 @@
 #'
 #' @examples examples
 algae_height <- function(Nf, spec_params) {
+  # Check that required parameters are supplied
+  if (is.na(spec_params['h_a'])) {abort_missing_parameter(param = "h_a", place = "spec_params")}
+  if (is.na(spec_params['h_b'])) {
+    h_b <- 1
+    rlang::inform(message = "h_b not supplied, defaulting to 1")
+  }
+  if (is.na(spec_params['h_c'])) {
+    h_c <- 0
+    rlang::inform(message = "h_c not supplied, defaulting to 0")
+  }
+  
   hm <- (Nf/spec_params['h_a'])^spec_params['h_b'] + spec_params['h_c']
   return(unname(hm))
 }
