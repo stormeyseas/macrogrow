a6b6a2209f492e220109b139bb0657314417527c
Tormey Reimer <tormeyreimer@gmail.com>
2024-07-08 16:39:19 +1000

    Uptake error message

---
+++ R/A_errors.R
@@ -0,0 +1,21 @@
+#' @title Custom error for missing parameters
+#'
+#' @param param missing parameter
+#' @param place place where parameter should be defined
+#'
+#' @export
+#'
+abort_missing_parameter <- function(param, place) {
+  if (is.null(place)) {
+    msg <- glue::glue("`{param}` must be defined")
+  } else {
+    msg <- glue::glue("`{param}` must be defined in {place}")
+  }
+  
+  rlang::abort("error_missing_parameter", 
+        message = msg, 
+        param = param, 
+        place = place
+  )
+}
+
