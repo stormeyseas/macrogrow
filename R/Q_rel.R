7e71111a1691b6f1ce4d3b0535224a398994033e
Tormey Reimer <tormeyreimer@gmail.com>
2024-07-15 15:18:36 +1000

    Close bracket

--- R/Q_rel.R
+++ R/Q_rel.R
@@ -12,7 +12,7 @@ Q_rel <- function(Q_int, spec_params) {
   if (is.na(spec_params['Q_min'])) {abort_missing_parameter(param = "Q_min", place = "spec_params")}
   if (is.na(spec_params['Q_max'])) {abort_missing_parameter(param = "Q_max", place = "spec_params")}
   if (unname(Q_int) < spec_params['Q_min']) {rlang::abort("Q too low! Q_int cannot be < Q_min!", class = "error_bad_parameter")}
-  if (unname(Q_int) > spec_params['Q_max']) {rlang::abort("Q too high! Q_int cannot be > Q_max!", class = "error_bad_parameter")}}
+  if (unname(Q_int) > spec_params['Q_max']) {rlang::abort("Q too high! Q_int cannot be > Q_max!", class = "error_bad_parameter")}
   
   Q_rel <- (spec_params['Q_max'] - Q_int)/(spec_params['Q_max'] - spec_params['Q_min'])
   return(unname(Q_rel))
