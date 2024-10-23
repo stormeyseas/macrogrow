c1470aff2b0915de7aa3dc708f507c16b51c4a14
Tormey Reimer <tormeyreimer@gmail.com>
2024-10-23 19:17:31 +1100

    New example data

--- data-raw/preprocess.R
+++ data-raw/preprocess.R
@@ -1,5 +1,13 @@
 # Load raw data from .csv file
-exampleData <- read.csv("data-raw/simulated-data.csv")
-# Apply preprocessing...
+df <- read.csv("data-raw/asparagopsis_armata.csv", row.names = 1)
+
+# Apply preprocessing
+## Check that units match what model needs, change if necessary
+df$unit[df$parameter == "h_max"]
+
+## Save as named vector
+asparagopsis <- df$value
+names(asparagopsis) <- df$param
+
 # Save the cleaned data in the required R package location
-usethis::use_data(exampleData)
+usethis::use_data(asparagopsis, overwrite = T)
