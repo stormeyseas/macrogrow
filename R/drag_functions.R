87270ef0389f4559b888aa078805323e4dbc62a8
Tormey Reimer <tormey.reimer@utas.edu.au>
2025-06-26 09:13:53 +1000

    Consolidate drag functions

--- R/drag_functions.R
+++ R/drag_functions.R
@@ -43,3 +43,32 @@ u_c <- function(U0, macro_state, site_params, spec_params, constants = c(s = 0.0
   }
   return(unname(u_c))
 }
+
+#' Relative water attenuation beneath canopy
+#'
+#' @inheritParams u_c
+#'
+#' @return a scalar of relative water attenuation beneath canopy
+#' @export
+#' 
+#' @examples examples
+u_b <- function(U0, macro_state, SA_WW = 0.5 * (0.0306/2), site_params, constants = c(s = 0.0045, gam = 1.13, a2 = 0.2^2, Cb = 0.0025)){
+  uc <- uc(U0, macro_state, SA_WW, site_params, constants)
+  Hc <- (site_params['d_top'] + site_params['hc']) / site_params['hz']
+  u_b <- (1 - u_c * Hc) / (1 - Hc)
+  return(unname(u_b))
+}
+
+#' Total drag coefficient
+#'
+#' @inheritParams u_c
+#' 
+#' @return The total drag coefficient C_t
+#' 
+C_t <- function(u_c, u_b, macro_state, SA_WW = 0.5 * (0.0306/2), site_params, constants = c(s = 0.0045, gam = 1.13, a2 = 0.2^2, Cb = 0.0025)) {
+  D <- SA_WW * min(macro_state['hm']/site_params['hc'], 1) * macro_state['biomass']
+  Kd <- 0.5 * site_params['hz'] * D * constants['s'] * U0^(constants['gam'] - 2)
+  Hc <- (site_params['d_top'] + site_params['hc']) / site_params['hz']
+  C_t <- (Kd * Hc * u_c ^ 2 + constants['Cb'] * u_b^2)
+  return(unname(C_t))
+}
