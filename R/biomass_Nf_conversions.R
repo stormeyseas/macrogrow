35a427cc67fd75478148456b8f62f3a112ddef6d
Tormey Reimer <tormey.reimer@utas.edu.au>
2025-10-30 10:35:45 +1100

    Add examples, fix params

--- R/biomass_Nf_conversions.R
+++ R/biomass_Nf_conversions.R
@@ -6,13 +6,14 @@
 #'
 #' @inheritParams Nf_to_biomass
 #' @inheritParams Q_rel 
-#' @param Q_rel the non-dimensionalised relative internal nutrient quotient (\eqn{Q_{rel}}). If neither of Q_int and Q_rel are provided the default Q_rel of 0.5 will be used.
+#' @param biomass starting biomass, mg m-3
+#' @param Q_rel the non-dimensionalised relative internal nutrient quotient (\eqn{Q_{rel}}). Only one of \eqn{Q_{int}} or \eqn{Q_{rel}} need to be provided. If neither \eqn{Q_{int}} or \eqn{Q_{rel}} are provided the default if \eqn{Q_{rel}=0.5} will be used.
 #' @param spec_params a vector of named numbers. Must include:
 #'  * `DWWW` (if dry = F), the conversion from dry weight to wet weight
 #'  * `Q_min`, the minimum internal nutrient quotient (mg gDW-1)
 #'  * `Q_max`, the maximum internal nutrient quotient
-#' @param biomass whether dry (default) or wet biomass is provided, mg m-3
-#'
+#' @param dry whether dry (default) or wet biomass is provided, mg m-3
+
 #' @return Nf, mg m-3
 #' @export
 #'
@@ -21,10 +22,10 @@
 #' starting_biomass <- 250 # mg m-3
 #' 
 #' # Using default Q_rel = 0.5
-#' \dontrun{Nf_to_biomass(biomass = starting_biomass, spec_params = my_species, dry = T)}
+#' \dontrun{biomass_to_Nf(biomass = starting_biomass, spec_params = my_species, dry = T)}
 #' 
 #' # Using a specific Q_int
-#' \dontrun{Nf_to_biomass(biomass = starting_biomass, Q_int = 30, spec_params = my_species, dry = T)}
+#' \dontrun{biomass_to_Nf(biomass = starting_biomass, Q_int = 30, spec_params = my_species, dry = T)}
 #' 
 #' @seealso [macrogrow::Nf_to_biomass()], [macrogrow::Q_rel()], [macrogrow::Q_int()]
 #' 
@@ -47,17 +48,21 @@ biomass_to_Nf <- function(biomass, Q_int = NULL, Q_rel = 0.5, spec_params, dry =
 #'
 #' @inheritParams Q_int
 #' @inheritParams Q_rel 
-#' @param dry logical, return dry or wet biomass. If dry = F, spec_params['DWWW'] must be provided
+#' @param dry logical, return dry or wet biomass. If dry = F, `spec_params['DWWW']` must be provided
 #'
+#' @details
+#' Not all parameters need to be provided to this function. 
+#' 
 #' @return dry (or wet) biomass, mg m-3
 #' @export
 #'
 #' @examples 
 #' my_species <- c(DWWW = 7.5, Q_min = 20, Q_max = 45)
-#' starting_biomass <- 250 # mg m-3
+#' starting_Nf <- 150 # mg m-3
+#' starting_Ns <- 50 # mg m-3
 #' 
 #' # Using default Q_rel = 0.5
-#' \dontrun{Nf_to_biomass(biomass = starting_biomass, spec_params = my_species, dry = T)}
+#' \dontrun{Nf_to_biomass(Nf = starting_Nf, Ns = starting_Ns, spec_params = my_species, dry = T)}
 #' 
 #' # Using a specific Q_int
 #' \dontrun{Nf_to_biomass(biomass = starting_biomass, Q_int = 30, spec_params = my_species, dry = T)}
